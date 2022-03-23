f472ec73998188037f189575c34082de4ee92e9a
jason926926 <100330060+jason926926@users.noreply.github.com>
2022-03-24 03:44:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,68 @@
 <html>
 <head>
-
+    <title>墾丁三日遊表單</title>
 </head>
 <body>
-<?php 
-    echo "hello,<B>php</B>";
-    echo "<br>";
-    $a=1;
-    $b=2;
-    echo $a+$b;
-?>
+    <body bgcolor=d6dce1>
+    <font size=40><center><p>墾丁三日遊</p></center></font>
+    <p><center><a href="https://bunnyann.com/kentingplay/"><img src="/img/a.jpg" width="500" height="250"></a>&nbsp
+    <a href="https://mimihan.tw/kenting-travel/"><img src="/img/b.jpg" width="500" height="250"></a></center></p></br>
+    </br>
+<form>
+    請輸入您的姓名:<input type="text"></br>
+    </br>請輸入您的e-mail:<input type="email"></br>
+    </br>請輸入您的電話:<input type="tel"></br>
+    </br>選擇您的性別:<input type="radio" name="gender" value="male">男生<input type="radio" name="gender" value="female">女生</br>
+    </br>選擇您的飲食偏好:<input type="checkbox" name="飲食" value="肉">肉<input type="checkbox" name="飲食" value="菜">菜<input type="checkbox" name="飲食" value="飯">飯<input type="checkbox" name="飲食" value="麵">麵</br>
+    </br>選擇衣服尺寸:
+    <select>
+        <option>S</option>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+        <option>XXL</option>
+        <option>XXXL</option>
+    </select>
+    </br>衣服尺寸對照表:
+    <table>
+        <tr>
+            <td>&nbsp</td>
+            <td>150~160cm</td>
+            <td>160~170cm</td>
+            <td>170~180cm</td>
+            <td>180~190cm</td>
+            <td>190cm以上</td>
+        </tr>
+        <tr>
+            <td>40~50kg</td>
+            <td>S</td>
+            <td>S</td>
+            <td>M</td>
+            <td>L</td>
+            <td>XL</td>
+        </tr>
+        <tr>
+            <td>50~65kg</td>
+            <td>S</td>
+            <td>M</td>
+            <td>L</td>
+            <td>L</td>
+            <td>XXL</td>
+        </tr>
+        <tr>
+            <td>65~80kg</td>
+            <td>M</td>
+            <td>L</td>
+            <td>XL</td>
+            <td>XXL</td>
+            <td>XXXL</td>
+        </tr>
+</table></br>
+</br>
+衣服的顏色:<input type="color"></br>
+</br>輸入您的生日:<input type="date"></br>
+</br>請輸入tickets數量:<input type="number"></br>
+</br><input type="submit" value="submit">&nbsp<input type="reset" value="reset"></br>
+</form>
 </body>
 </html>
