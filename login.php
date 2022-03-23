fe42c38ef58d48162d9a8177af9339ea7c134b19
jason926926 <100330060+jason926926@users.noreply.github.com>
2022-03-24 03:48:27 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+    <center><font size=40 face="標楷體"><p>登入<p></font>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        請輸入您的帳號:<input type="username" name="user"></br>
+        </br>請輸入您的密碼:<input type="password" name="password"></br>
+        </br><input type="submit" name="submit" value="login">
+    </form>
+</body>
+</html>
