9127dc18a904afd9c935d7d8e26e03140cd72166
jason926926 <100330060+jason926926@users.noreply.github.com>
2022-03-24 03:49:34 +0800

    Add files via upload

---
+++ sign.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>sign up</title>
+</head>
+<body>
+    <center><font size=40><p>註冊</p></font>
+    <form action="information.php" method="post" enctype="multipart/form-data">
+        請輸入您的帳號:<input type="username" name="user"></br>
+        </br>請輸入您的密碼:<input type="password" name="password"></br>
+        </br>請輸入您的電子郵件:<input type="email" name="email"></br>
+        </br>請輸入您的電話:<input type="tel" name="tel"></br>
+        </br><input type="submit" name="submit" value="確認送出">&nbsp<input type="reset" value="重新填寫"></br>
+    </form>
+    </center>
+</body>
+</html>
